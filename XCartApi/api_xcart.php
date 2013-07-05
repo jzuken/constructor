5231c3796c79252e63087821feec7abed0cd4d7e
NikitaBumakov <nikita.bumakov@gmail.com>
2013-07-05 16:08:49 +0400

    some refactoring

--- XCartApi/api_xcart.php
+++ XCartApi/api_xcart.php
@@ -1,28 +1,27 @@
 <?php
 
-require './api_settings.php';
+require_once './api_settings.php';
 
 header('Content-type: text/xml');
 
-mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD)
-or die("<p>Ошибка подключения к базе данных: " . mysql_error() . "</p>");
+mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD) or die("<p>" . mysql_error() . "</p>");
 mysql_select_db(DATABASE_NAME);
 
 $request = $_REQUEST['request'];
 switch ($request) {
-    case 'users':
+    case 'users_count':
         getUsersCount();
         break;
 }
 
-function getUsersCount(){
-    $sql = 'select * from xcart_customers;';
+function getUsersCount()
+{
+    global $sql_tbl;
+    $sql = "SELECT * FROM $sql_tbl[customers];";
     $query = mysql_query($sql) or die(mysql_error());
-    $count = mysql_num_rows($query) ;
+    $count = mysql_num_rows($query);
 
     $xml = new SimpleXMLElement('<users/>');
-    $xml->addAttribute(count, $count);
+    $xml->addAttribute('count', $count);
     echo $xml->asXML();
 }
-
-?>
