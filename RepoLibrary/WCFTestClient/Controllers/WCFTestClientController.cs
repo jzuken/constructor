d3e267e9a6a43c52abd58aaaf706028281e32cfc
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-07-08 20:58:45 +0400

    Test binding to WCF service

--- RepoLibrary/WCFTestClient/Controllers/WCFTestClientController.cs
+++ RepoLibrary/WCFTestClient/Controllers/WCFTestClientController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.ServiceModel;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,19 @@ namespace WCFTestClient.Controllers
 {
     public class WCFTestClientController : Controller
     {
+        WCFTestReference.RepoLibraryClient wcfClient;
+        public WCFTestClientController()
+        {
+            this.wcfClient = new WCFTestReference.RepoLibraryClient("WSHttpBinding_IRepoLibrary");
+        }
+
         //
         // GET: /WCFTestClient/
 
         public ActionResult Index()
         {
+            WCFTestReference.Project project = this.wcfClient.GetProject(0);
+            ViewData["projectName"] = project.Name;
             return View();
         }
 
