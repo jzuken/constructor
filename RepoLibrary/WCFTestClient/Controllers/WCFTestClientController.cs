1b3387709bd2dcf9f94df78ba742b37b9b8ecf9e
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-07-09 00:32:57 +0400

    Test reading and writing data via WCF

--- RepoLibrary/WCFTestClient/Controllers/WCFTestClientController.cs
+++ RepoLibrary/WCFTestClient/Controllers/WCFTestClientController.cs
@@ -26,5 +26,39 @@ namespace WCFTestClient.Controllers
             return View();
         }
 
+        public ActionResult AddProject(string projectId, string projectName)
+        {
+            int id = Convert.ToInt32(projectId);
+            WCFTestReference.Project project = new WCFTestReference.Project();
+            project.Id = id;
+            project.Name = projectName;
+            string result = wcfClient.SaveProject(project);
+            if (result == "ok")
+            {
+                ViewData["projectId"] = id;
+                ViewData["projectName"] = projectName;
+            }
+            else
+            {
+                RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult GetProject(string projectId)
+        {
+            int id = Convert.ToInt32(projectId);
+            WCFTestReference.Project project = wcfClient.GetProject(id);
+            if (project != null)
+            {
+                ViewData["projectId"] = project.Id;
+                ViewData["projectName"] = project.Name;
+            }
+            else
+            {
+                RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
