badc7ee8252e198da855710368fb35a8297bfd30
NikitaBumakov <nikita.bumakov@gmail.com>
2013-07-13 23:08:22 +0400

    json support

--- XCartApi/api.php
+++ XCartApi/api.php
@@ -5,48 +5,62 @@
  */
 require './xcart/top.inc.php';
 require './xcart/init.php';
+
 /*
  * Production imports
  */
 //require '../top.inc.php';
 //require '../init.php';
 
-header('Content-type: text/xml');
-
-$request = $_GET['request'];
 mysql_connect($sql_host, $sql_user, $sql_password)  or die(mysql_error());
 mysql_select_db($sql_db) or die(mysql_error());
 
+
+$type = $_GET['type'];
+if (is_null($type)) {
+    $type = "xml";
+}
+if ($type == "xml") {
+    header('Content-type: text/xml');
+}
+
 $users_fields = array(id, login, username, usertype, password, signature, invalid_login_attempts, title, firstname, lastname, company, email, url, last_login, first_login, status, activation_key, autolock, suspend_date, referer, ssn, language, cart, change_password, change_password_date, parent, pending_plan_id, activity, membershipid, pending_membershipid, tax_number, tax_exempt, trusted_provider, cookie_access);
 $orders_fields = array(orderid, userid, membership, total, giftcert_discount, giftcert_ids, subtotal, discount, coupon, coupon_discount, shippingid, shipping, tracking, shipping_cost, tax, taxes_applied, date, status, payment_method, flag, notes, details, customer_notes, customer, title, firstname, lastname, company, url, email, language, clickid, membershipid, paymentid, payment_surcharge, tax_number, tax_exempt, init_total, access_key, klarna_order_status);
+$discounts_fields = array(discountid, minprice, discount, discount_type, provider);
 
-switch ($request) {
-    case 'users_count':
-        $xml = get_users_count();
-        break;
-    case 'users':
-        $xml = get_users($users_fields);
-        break;
-    case 'orders':
-        $xml = get_orders($orders_fields, $_REQUEST['from'], $_REQUEST['to']);
-        break;
-    case 'last_order':
-        $xml = get_last_order($orders_fields);
-        break;
-    case 'discounts':
-        $xml = get_discounts();
-        break;
-    default:
-        $xml = new SimpleXMLElement('<error/>');
-        break;
+echo get_response($type);
+
+function get_response($type)
+{
+    global $users_fields, $orders_fields, $discounts_fields;
+    $request = $_GET['request'];
+    switch ($request) {
+        case 'users_count':
+            $response = get_users_count();
+            break;
+        case 'users':
+            $response = get_users($users_fields);
+            break;
+        case 'orders':
+            $response = get_orders($orders_fields, $_GET['from'], $_GET['to'], $type);
+            break;
+        case 'last_order':
+            $response = get_last_order($orders_fields, $type);
+            break;
+        case 'discounts':
+            $response = get_discounts($discounts_fields);
+            break;
+        default:
+            $response = new SimpleXMLElement('<error/>');
+            break;
+    }
+    return $response;
 }
-echo $xml->asXML();
 
 function get_users_count()
 {
     global $sql_tbl;
-    $sql = "SELECT * FROM $sql_tbl[customers];";
-    $query = mysql_query($sql) or die(mysql_error());
+    $query = mysql_query("SELECT * FROM $sql_tbl[customers]");
     $count = mysql_num_rows($query);
     $xml = new SimpleXMLElement('<users/>');
     $xml->addAttribute('count', $count);
@@ -56,8 +70,7 @@ function get_users_count()
 function get_users($fields)
 {
     global $sql_tbl;
-    $sql = "SELECT * FROM $sql_tbl[customers];";
-    $query = mysql_query($sql) or die(mysql_error());
+    $query = mysql_query("SELECT * FROM $sql_tbl[customers]");
     $count = mysql_num_rows($query);
     $xml = new SimpleXMLElement('<users/>');
     $xml->addAttribute('count', $count);
@@ -71,55 +84,81 @@ function get_users($fields)
     return $xml;
 }
 
-function get_orders($fields, $from, $to)
+function get_orders($fields, $from, $to, $type)
 {
-    if(is_null($from)){
+    if (is_null($from)) {
         $from = 0;
     }
-    if(is_null($to)){
+    if (is_null($to)) {
         $to = XC_TIME;
     }
     global $sql_tbl;
     $date_condition = "date BETWEEN $from AND $to";
-    $sql_orders = "SELECT * FROM $sql_tbl[orders] WHERE $date_condition;";
-    $query = mysql_query($sql_orders) or die(mysql_error());
-    $xml = new SimpleXMLElement('<orders/>');
-    while ($row = mysql_fetch_array($query)) {
-        $order = $xml->addChild('order');
-        foreach ($fields as &$value) {
-            $order->addChild($value, $row[$value]);
-        }
+    $query = mysql_query("SELECT * FROM $sql_tbl[orders] WHERE $date_condition");
+
+    switch ($type) {
+        case "json":
+            $response = get_json($fields, $query);
+            break;
+        default:
+            $response = get_xml($fields, $query, 'order');
+            break;
     }
-    return $xml;
+    return $response;
 }
 
-function get_last_order($fields)
+function get_last_order($fields, $type)
 {
     global $sql_tbl;
-    $sql = "SELECT * FROM $sql_tbl[orders] ORDER BY date DESC LIMIT 1";
-    $query = mysql_query($sql) or die(mysql_error());
-    $xml = new SimpleXMLElement('<orders/>');
-    while ($row = mysql_fetch_array($query)) {
-        $order = $xml->addChild('order');
-        foreach ($fields as &$value) {
-            $order->addChild($value, $row[$value]);
-        }
+    $query = mysql_query("SELECT * FROM $sql_tbl[orders] ORDER BY date DESC LIMIT 1");
+
+    switch ($type) {
+        case "json":
+            $response = get_json($fields, $query);
+            break;
+        default:
+            $response = get_xml($fields, $query, 'order');
+            break;
     }
-    return $xml;
+    return $response;
 }
 
-function get_discounts()
+
+function get_discounts($fields)
 {
     global $sql_tbl;
-    $sql = "SELECT * FROM $sql_tbl[discounts]";
-    $query = mysql_query($sql) or die(mysql_error());
+    $query = mysql_query("SELECT * FROM $sql_tbl[discounts]") or die(mysql_error());
     $xml = new SimpleXMLElement('<discounts/>');
     while ($row = mysql_fetch_array($query)) {
         $discount = $xml->addChild('discount');
-        $fields = array(discountid, minprice, discount, discount_type, provider);
         foreach ($fields as &$value) {
             $discount->addChild($value, $row[$value]);
         }
     }
     return $xml;
+}
+
+function get_json($fields, $query)
+{
+    $json_result = array();
+    while ($row = mysql_fetch_array($query)) {
+        $order_json = array();
+        foreach ($fields as &$value) {
+            $order_json[$value] = $row[$value];
+        }
+        array_push($json_result, $order_json);
+    }
+    return json_encode($json_result);
+}
+
+function get_xml($fields, $query, $row_title)
+{
+    $xml = new SimpleXMLElement('<root/>');
+    while ($row = mysql_fetch_array($query)) {
+        $order = $xml->addChild($row_title);
+        foreach ($fields as &$value) {
+            $order->addChild($value, $row[$value]);
+        }
+    }
+    return $xml->asXML();
 }
