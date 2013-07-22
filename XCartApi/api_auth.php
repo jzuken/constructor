6a6d371729f0d78941d51d5a02db47be63ee89b4
NikitaBumakov <428kr9t6567v>
2013-07-22 15:37:15 +0400

    auth test

---
+++ XCartApi/api_auth.php
@@ -0,0 +1,2 @@
+<?php echo htmlspecialchars($_POST['name']); ?>
+<?php echo $_POST['pass']; ?>
---
+++ XCartApi/auth_test.php
@@ -0,0 +1,5 @@
+<form action="api_auth.php" method="post" >
+    <p>Name:<input type="text" name="name" /></p>
+    <p>Pass:<input type="text" name="pass" /></p>
+    <p><input type="submit" /></p>
+</form>
