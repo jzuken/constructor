9afe2a005cb1f8c24ae831ec9a4083bbd231e336
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-07-30 02:31:18 +0400

    users design changes

--- Android/AdminShop/src/com/example/adminshop/Users.java
+++ Android/AdminShop/src/com/example/adminshop/Users.java
@@ -1,35 +1,22 @@
 package com.example.adminshop;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Users extends PinSupportActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.users);
+		setContentView(R.layout.users_search);
+		registered = (TextView) findViewById(R.id.registered);
+		online = (TextView) findViewById(R.id.online);
 
-		LayoutInflater inflater = LayoutInflater.from(this);
-		List<View> pages = new ArrayList<View>();
-
-		View page = inflater.inflate(R.layout.users_search, null);
-		pages.add(page);
-
-		SwipingPagerAdapter pagerAdapter = new SwipingPagerAdapter(pages);
-		ViewPager viewPager = (ViewPager) findViewById(R.id.users_view_pager);
-		viewPager.setAdapter(pagerAdapter);
-		viewPager.setCurrentItem(0);
-
-		final EditText searchLine = (EditText) page.findViewById(R.id.usersSearchLine);
+		EditText searchLine = (EditText) findViewById(R.id.usersSearchLine);
 		searchLine.setOnKeyListener(new OnKeyListener() {
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
 				if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
@@ -39,4 +26,7 @@ public class Users extends PinSupportActivity {
 			}
 		});
 	}
+	
+	private TextView registered;
+	private TextView online;
 }
