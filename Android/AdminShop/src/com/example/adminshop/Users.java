fc0892137acb0c2bcdb20451f4fe825c00d8a549
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-08-02 03:42:07 +0400

    new checkbox design, removed search line from users

--- Android/AdminShop/src/com/example/adminshop/Users.java
+++ Android/AdminShop/src/com/example/adminshop/Users.java
@@ -1,10 +1,6 @@
 package com.example.adminshop;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnKeyListener;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class Users extends PinSupportActivity {
@@ -15,16 +11,6 @@ public class Users extends PinSupportActivity {
 		setContentView(R.layout.users_search);
 		registered = (TextView) findViewById(R.id.registered);
 		online = (TextView) findViewById(R.id.online);
-
-		EditText searchLine = (EditText) findViewById(R.id.usersSearchLine);
-		searchLine.setOnKeyListener(new OnKeyListener() {
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-					return true;
-				}
-				return false;
-			}
-		});
 	}
 	
 	private TextView registered;
