3df10b908eb1f9cc568d9bdfa64ac176a2c11bd7
vladimir-zakharov <vladimir.zakharov93@gmail.com>
2013-08-07 04:53:44 +0400

    added pull to refresh, on demand download to users, new library (for pull to refresh), gross total and total paid to orders info

--- Android/AdminShop/src/com/example/adminshop/Users.java
+++ Android/AdminShop/src/com/example/adminshop/Users.java
@@ -8,12 +8,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 public class Users extends PinSupportNetworkActivity {
 
@@ -21,23 +31,24 @@ public class Users extends PinSupportNetworkActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.users_search);
-		progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 		setupListViewAdapter();
+		setupSortSpinner();
 	}
 
 	@Override
 	protected void withoutPinAction() {
+		clearData();
 		updateUsersList();
 	}
-	
+
 	public void showMoreClick(View v) {
-		
+
 	}
 
 	private void updateUsersList() {
-		clearData();
 		progressBar.setVisibility(View.VISIBLE);
-		showMoreButton.setVisibility(View.GONE);
+		isDownloading = true;
+		hasNext = false;
 		GetRequester dataRequester = new GetRequester() {
 			@Override
 			protected void onPostExecute(String result) {
@@ -47,17 +58,20 @@ public class Users extends PinSupportNetworkActivity {
 						JSONObject userCount = responseObj.getJSONObject("users_count");
 						registered.setText(userCount.getString("registered"));
 						JSONArray array = responseObj.getJSONArray("users");
-						for (int i = 0; i < array.length(); i++) {
+						int length = array.length();
+						if (length == packAmount) {
+							hasNext = true;
+						}
+						for (int i = 0; i < length; i++) {
 							JSONObject obj = array.getJSONObject(i);
 							String name = obj.getString("title") + " " + obj.getString("firstname") + " "
 									+ obj.getString("lastname");
 							String login = obj.getString("login");
 							String type = obj.getString("usertype");
 							String lastLogin = getFormatDate(Long.parseLong(obj.getString("last_login")));
-							String totalOrder = obj.getString("orders_count");
+							String totalOrder = obj.optString("orders_count");
 							addUserToList(name, login, type, lastLogin, totalOrder);
 						}
-						showMoreButton.setVisibility(View.VISIBLE);
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
@@ -65,16 +79,21 @@ public class Users extends PinSupportNetworkActivity {
 					showConnectionErrorMessage();
 				}
 				progressBar.setVisibility(View.GONE);
+				usersListView.onRefreshComplete();
+				isDownloading = false;
 			}
 		};
 
-		dataRequester.execute("http://54.213.38.9/xcart/api.php?request=users&from=0&size=10&sort=orders");
+		dataRequester.execute("http://54.213.38.9/xcart/api.php?request=users&from=" + String.valueOf(currentAmount)
+				+ "&size=" + String.valueOf(packAmount) + "&sort=" + getCurrentSort());
+		currentAmount += packAmount;
 	}
-	
+
 	private void clearData() {
 		adapter.clear();
 		registered.setText("");
 		online.setText("");
+		currentAmount = 0;
 	}
 
 	private String getFormatDate(Long seconds) {
@@ -96,6 +115,21 @@ public class Users extends PinSupportNetworkActivity {
 		}
 	}
 
+	private String getCurrentSort() {
+		switch (currentSortOption) {
+		case 0:
+			return "login_date";
+		case 1:
+			return "order_date";
+		case 2:
+			return "orders";
+		case 3:
+			return "none";
+		default:
+			return null;
+		}
+	}
+
 	private void addUserToList(final String name, final String login, final String type, final String lastLogin,
 			final String totalOrders) {
 		adapter.add(new User(name, login, type, lastLogin, totalOrders));
@@ -103,26 +137,85 @@ public class Users extends PinSupportNetworkActivity {
 
 	private void setupListViewAdapter() {
 		adapter = new UsersListAdapter(this, R.layout.user_item, new ArrayList<User>());
-		ListView usersListView = (ListView) findViewById(R.id.users_list);	
+		usersListView = (PullToRefreshListView) findViewById(R.id.users_list);
 		LayoutInflater inflater = getLayoutInflater();
-		
+
 		View listHeader = inflater.inflate(R.layout.users_header, null, false);
 		registered = (TextView) listHeader.findViewById(R.id.registered);
 		online = (TextView) listHeader.findViewById(R.id.online);
-		usersListView.addHeaderView(listHeader, null, false);
-		
+		usersListView.getRefreshableView().addHeaderView(listHeader, null, false);
+
 		View listFooter = inflater.inflate(R.layout.users_footer, null, false);
-		showMoreButton = (Button) listFooter.findViewById(R.id.show_more_button);
-		usersListView.addFooterView(listFooter, null, false);
-		
-		usersListView.setHeaderDividersEnabled(false);
-		usersListView.setFooterDividersEnabled(false);
+		progressBar = (ProgressBar) listFooter.findViewById(R.id.progress_bar);
+		usersListView.getRefreshableView().addFooterView(listFooter, null, false);
+
+		usersListView.getRefreshableView().setHeaderDividersEnabled(false);
+		usersListView.getRefreshableView().setFooterDividersEnabled(false);
+
+		usersListView.setOnScrollListener(new OnScrollListener() {
+
+			@Override
+			public void onScrollStateChanged(AbsListView arg0, int arg1) {
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+				if (totalItemCount > 4 && firstVisibleItem + visibleItemCount == totalItemCount && !isDownloading
+						&& hasNext) {
+					Log.d("scroll", String.valueOf(totalItemCount));
+					updateUsersList();
+				}
+			}
+		});
+
+		usersListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				clearData();
+				updateUsersList();
+			}
+		});
+
 		usersListView.setAdapter(adapter);
 	}
 
+	private void setupSortSpinner() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sortOptions);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		Spinner spinner = (Spinner) findViewById(R.id.sort_spinner);
+		spinner.setAdapter(adapter);
+		spinner.setSelection(0);
+		currentSortOption = 0;
+
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				currentSortOption = position;
+				clearData();
+				if (isFirstSelection) {
+					isFirstSelection = false;
+				} else {
+					updateUsersList();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+	}
+
+	private boolean isFirstSelection = true;
 	private TextView registered;
 	private TextView online;
 	private ProgressBar progressBar;
 	private UsersListAdapter adapter;
-	private Button showMoreButton;
+	private int currentAmount;
+	private boolean isDownloading;
+	private final String[] sortOptions = { "Last login", "Last order", "Total orders", "None" };
+	private int currentSortOption;
+	private boolean hasNext;
+	private int packAmount = 10;
+	PullToRefreshListView usersListView;
 }
