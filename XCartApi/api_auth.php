471b06c2cff7e0bc138a219776e5f82c77317533
NikitaBumakov <nikita.bumakov@gmail.com>
2013-08-13 11:32:18 +0400

    auth test

--- XCartApi/api_auth.php
+++ XCartApi/api_auth.php
@@ -1,29 +1,29 @@
 <?php
-require './xcart/top.inc.php';
-require './xcart/init.php';
+//require './xcart/top.inc.php';
+//require './xcart/init.php';
+//
+//require './xcart/include/login.php';
+//require_once './xcart/include/classes/class.XCPasswordHash.php';
 
-require './xcart/include/login.php';
-require './xcart/include/common.php';
-require_once './xcart/include/classes/class.XCPasswordHash.php';
+require './top.inc.php';
+require './init.php';
 
-//echo htmlspecialchars($_POST['name']);
-//echo $_POST['pass'];
-
-
+require './include/login.php';
+require_once './include/classes/class.XCPasswordHash.php';
 
 $username = trim($_POST['name']);
 $password = trim($_POST['pass']);
 
-$usertype = 'P';//TODO: P - in simple mode? in real xcart - A
+$usertype = 'P'; //TODO: P - in simple mode? in real xcart - A
 $query = "SELECT * FROM $sql_tbl[customers] WHERE login='$username' AND usertype='$usertype'";
 $user_data = func_query_first($query);
-echo empty($user_data['usertype']);
 $allow_old_password_format = true;
 $password = trim(stripslashes($password));
-$right_hash = text_decrypt($crypted);
+$right_hash = text_decrypt($user_data['password']);
 $t_hasher = new XCPasswordHash();
-print_r($t_hasher);
 $is_correct = $t_hasher->CheckPassword($password, $right_hash);
-echo $is_correct;
-print_r($is_correct);
-echo '<br>';
+if($is_correct){
+    echo '<p>correct!</p>';
+}else{
+    echo '<p>WRONG!</p>';
+}
