209a850db5b028eefbaada03c56ae71d61965c4b
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-08-25 23:12:20 +0400

    WebAPI service for getting shop settings

---
+++ RepoLibrary/ApplicationServerListener/Controllers/ShopsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApplicationServerListener.Controllers
+{
+    public class ShopsController : ApiController
+    {
+        RepoLibraryReference.RepoLibraryClient wcfClient = new RepoLibraryReference.RepoLibraryClient("WSHttpBinding_IRepoLibrary");
+        // GET api/values/5
+        public string Get(string name)
+        {
+            RepoLibraryReference.Project project = wcfClient.GetProject(@name);
+            if (project == null)
+            {
+                return "NOT FOUND";
+            }
+            else
+            {
+                return project.Settings;
+            }
+        }
+    }
+}
