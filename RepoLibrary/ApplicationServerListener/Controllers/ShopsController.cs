40f7788604980a041e69ade4236cd64d19b5a14c
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2013-10-10 15:30:19 +0400

    Getting application-server URL

--- RepoLibrary/ApplicationServerListener/Controllers/ShopsController.cs
+++ RepoLibrary/ApplicationServerListener/Controllers/ShopsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Http;
 
 namespace ApplicationServerListener.Controllers
@@ -10,6 +13,20 @@ namespace ApplicationServerListener.Controllers
     public class ShopsController : ApiController
     {
         RepoLibraryReference.RepoLibraryClient wcfClient = new RepoLibraryReference.RepoLibraryClient("WSHttpBinding_IRepoLibrary");
+        
+        private string CreateMD5Hash(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        
         // GET api/values/5
         [ActionName("DefaultAction")]
         public HttpResponseMessage Get(string name)
@@ -55,6 +72,52 @@ namespace ApplicationServerListener.Controllers
                     return new HttpResponseMessage() { Content = new StringContent("{\"subscribed\": \"none\"}") };
                 }
             }
+        }
+
+        [ActionName("ApiURL")]
+        public HttpResponseMessage GetApiURL(string name)
+        {
+            NameValueCollection getParams = Request.RequestUri.ParseQueryString();
+            if (getParams["key"] != null)
+            {
+                string key = getParams["key"].ToString();
+                string hash = this.CreateMD5Hash(key);
+                RepoLibraryReference.Project project = wcfClient.GetProject(@name);
+                if (project == null)
+                {
+                    return new HttpResponseMessage() { Content = new StringContent("{}") };
+                }
+                else
+                {
+                    if (project.keyHash == hash)
+                    {
+                        string expirationDate = project.ExpirationDate;
+                        DateTime todate = DateTime.Today;
+                        DateTime expiring;
+                        bool parsed = DateTime.TryParse(expirationDate, out expiring);
+                        if (parsed)
+                        {
+                            if (DateTime.Compare(todate, expiring) > 0)
+                            {
+                                return new HttpResponseMessage() { Content = new StringContent("{}") };
+                            }
+                            else
+                            {
+                                return new HttpResponseMessage() { Content = new StringContent("{\"apiUrl\": \"" + project.apiUrl + "\"}") };
+                            }
+                        }
+                        else
+                        {
+                            return new HttpResponseMessage() { Content = new StringContent("{}") };
+                        }
+                    }
+                    else
+                    {
+                        return new HttpResponseMessage() { Content = new StringContent("{}") };
+                    }
+                }
+            }
+            return new HttpResponseMessage() { Content = new StringContent("{}") };
         }
     }
 }
